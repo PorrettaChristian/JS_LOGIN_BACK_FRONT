25529669dd4f9e075e724606dda87f6d100eb07a
Porretta Christian <porretta.christian@osdbsesto>
2022-02-24 10:49:19 +0100

    add readFile

--- back/index.js
+++ back/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var cors = require("cors");
 var apiServer = express();
 apiServer.use(cors());
+var fs = require("fs");
 
 var host = "localhost";
 var port = 5000;
@@ -12,10 +13,19 @@ apiServer.listen(port, host, ()=>{
 
 apiServer.get("/api/login", (req , res) => {
     console.log("ricevuti" , req.query.mail , req.query.psw);
-    if (req.query.mail === "chri" && req.query.psw === "porr") {
-        res.sendStatus(200)
-    } else {
-        res.sendStatus(400)
-    }
-    
+    fs.readFile("users.json", (err, data) => {
+        if (err) {
+            console.log("error: " + err);
+            res.status(500).json({message: "errore generico"});
+        } else {
+            var users = JSON.parse(data);
+            var mail = users.find(x => x.mail  === req.query.mail)
+            var psw = users.find(x => x.psw  === req.query.psw)
+            if (mail && psw) {
+                res.status(200).json({message: "login effettuato"});
+            } else {
+                res.status(400).json({message: "login non effettuato"});
+            }
+        }
+    });
 })

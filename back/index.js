683b651bfd1554d57a443e8635083db4175e912a
Porretta Christian <porretta.christian@osdbsesto>
2022-03-03 11:18:11 +0100

    add mysql2

--- back/index.js
+++ back/index.js
@@ -3,7 +3,15 @@ var cors = require("cors");
 var apiServer = express();
 apiServer.use(cors());
 var fs = require("fs");
+const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+    host: 'porretta.christian.tave.osdb.it',
+    user: 'c189_christian',
+    password: "INSERT PSW",
+    database: 'c189_5AI_2021',
+})
+  
 var host = "localhost";
 var port = 5000;
 
@@ -15,35 +23,31 @@ apiServer.listen(port, host, ()=>{
 
 apiServer.get("/api/login", (req , res) => {
     console.log("ricevuti" , req.query.mail , req.query.psw);
-    fs.readFile("users.json", (err, data) => {
-        if (err) {
-            console.log("error: " + err);
-            res.status(500).json({message: "errore generico"});
-        } else {
-            var users = JSON.parse(data);
-            var mail = users.find(x => x.mail  === req.query.mail)
-            var psw = users.find(x => x.psw  === req.query.psw)
-            if (mail && psw) {
-                res.status(200).json({message: "login effettuato"});
+    connection.query(
+        'SELECT * FROM `users` WHERE `mail` = ? AND `psw` = ?',
+        [req.query.mail, req.query.psw],
+        function(err, results, fields) {
+            if (results.length === 0) {
+                res.status(400).json({message: "log non effettuato"});
             } else {
-                res.status(400).json({message: "login non effettuato"});
+                res.status(200).json({message: "log effettuato"});
             }
         }
-    });
+      );
 })
 
 apiServer.get("/api/reg", (req , res) => {
     console.log("ricevuti" , req.query.mail , req.query.psw);
-    var str = {"mail" : req.query.mail, "psw" : req.query.psw};
-    array.push(str);
-    fs.writeFile("users.json", JSON.stringify(array), (err) =>{
-        if(err){
+    connection.query(
+        "INSERT INTO users (mail, psw) VALUES (?,?);",
+        [req.query.mail, req.query.psw],
+        function(err, results, fields) {
             console.log(err);
-            res.status(400).json({message: "reg non effettuato"});
+            if (err) {
+                res.status(400).json({message: "reg non effettuato"});
+            } else {
+                res.status(200).json({message: "reg effettuato"});
+            }
         }
-        else{
-            console.log("ho scritto");
-            res.status(200).json({message: "reg effettuato"});
-        }
-    });
+      );
 })
